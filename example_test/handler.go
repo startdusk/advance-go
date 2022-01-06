00aea4e9b0de37559235f4ed47230966ef2d9659
startdusk <522383345@qq.com>
2022-01-06 23:01:20 +0800

    add(example_test): add example_test

---
+++ example_test/handler.go
@@ -0,0 +1,7 @@
+package exampletest
+
+import "fmt"
+
+func Println(s string) {
+	fmt.Println(s)
+}
---
+++ example_test/handler_example_test.go
@@ -0,0 +1,9 @@
+package exampletest
+
+// 范例测试，用于文档，也用来测试
+// go test -run ExampleSendJSON
+func ExamplePrintln() {
+	Println("123")
+	// Output:
+	// 123
+}
